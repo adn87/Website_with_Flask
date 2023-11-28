3f616e1b04137dbcbdfa7c924dd16d391bf48771
adn87 <aaghadieh@gmail.com>
2023-11-28 12:13:04 -0500

    Website_with_Flask_005
    - Using Python Decorators to Style HTML Tags.

--- hello.py
+++ hello.py
@@ -3,6 +3,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+def make_italic(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
     return ('<h1 style="text-align: center">Hello, World!</h1>'
@@ -12,6 +33,9 @@ def hello_world():
 
 # Different routes using the app.route decorator.
 @app.route("/bye")
+@make_bold
+@make_underline
+@make_italic
 def bye():
     return "Bye"
 
