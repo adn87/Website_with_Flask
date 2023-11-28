64845b09d0831ff32db3d94bd8a7c4a568708f04
adn87 <aaghadieh@gmail.com>
2023-11-28 11:58:56 -0500

    Website_with_Flask_004
    - Rendering HTML Elements with Flask.

--- hello.py
+++ hello.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return ('<h1 style="text-align: center">Hello, World!</h1>'
+            '<p>This is a paragraph</p>'
+            '<img src="https://media.giphy.com/media/1wnZSnmrnwJmnJkd1c/giphy-downsized-large.gif" width=200px>')
 
 
+# Different routes using the app.route decorator.
 @app.route("/bye")
 def bye():
     return "Bye"
 
 
+# Creating variable paths and converting the path to a specified data type.
 @app.route("/<name>/<int:number>")
 def greet(name, number):
     return f"Hello {name}! You are {number} years old"
 
 
 if __name__ == "__main__":
+    # Run the app in debug mode to auto-reload
     app.run(debug=True)
