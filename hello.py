526ffebf59340a8be64f476348f1c464d738592b
adn87 <aaghadieh@gmail.com>
2023-11-28 11:23:23 -0500

    Website_with_Flask_002
    - learning how to create a python decorator function and how they work.

--- hello.py
+++ hello.py
@@ -1,8 +1,44 @@
-from flask import Flask
+# from flask import Flask
+import time
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello_world():
+#     return "Hello, World!"
+#
+#
+# if __name__ == "__main__":
+#     app.run()
 
-app = Flask(__name__)
+
+# Creating a python decorator function
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        # Do something before
+        function()
+        # Do something after
+
+    return wrapper_function
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@delay_decorator
+def say_hello():
+    print("hello")
+
+
+@delay_decorator
+def say_bye():
+    print("bye")
+
+
+def say_greeting():
+    print("how are you?")
+
+
+# this will delay 2 seconds
+say_hello()
+# this will be triggered right away
+say_greeting()
